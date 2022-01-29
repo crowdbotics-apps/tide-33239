eabcbc80ff965b90d8f6970c28dcd09e9855aa89
Crowdbotics <team@crowdbotics.com>
2022-01-29 21:54:27 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/tide_33239/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tide_33239;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "tide_33239";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "tide_33239",
+  url: "https://tide_33239.botics.co",
+  api: "https://tide_33239.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
